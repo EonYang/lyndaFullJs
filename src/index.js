2ea449694d51c67eba76d92c1439748cbe557111
yangyang <yangeon@163.com>
2018-02-14 20:59:40 -0500

    day1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const color = Math.random() > 0.5 ? "green" : "red" ;
+
+ReactDom.render(
+  <h1>
+  JAX!!s!!asmdmaklsmdkla
+  asdkjaskfd
+  asdfasdfsdf
+
+  </h1>,
+  document.getElementById('root')
+);
